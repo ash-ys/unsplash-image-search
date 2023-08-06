378cfba38f76a026175a50602dd4418f046157ee
Ashish Wagle <Awagle4@gmail.com>
2023-08-06 13:25:49 +0545

    ...

--- unsplash-react/src/App.jsx
+++ unsplash-react/src/App.jsx
@@ -1,16 +1,33 @@
+import { createContext, useState } from "react";
+import Images from "./components/Images";
+import Bar from "./components/bar";
+import SearchField from "./components/SearchField";
+import useAxios from "./hooks/useAxios";
 
-import './index.css'
-import Bar from './components/bar'
-import Searchfield from './components/Searchfield'
-import Images from './components/Images'
+// Create Context
+export const ImageContext = createContext();
+
 function App() {
-  return (
-      <>
-      <Bar><Searchfield /></Bar>
-      <Images />
-      </>
+  const [searchImage, setSearchImage] = useState('');
+  const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=cats&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
 
-  )
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData,
+    searchImage,
+    setSearchImage
+  }
+
+  return (
+    <ImageContext.Provider value={value}>
+      <Bar>
+        <SearchField />
+      </Bar>
+      <Images />
+    </ImageContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- unsplash-react/src/components/bar.jsx
+++ unsplash-react/src/components/bar.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-const bar = ({ children }) => {
+const Bar = ({ children }) => {
   return (
-    <div className='flex items-center py-10' style={{ backgroundColor: '#4B89EC', padding: '20px' }}>
+    <div className="bg-gray-900 flex items-center py-10">
       <div className='max-w-md mx-auto w-full'>
-      <h1 className='text-white text-center text-2xl font-bold mb-5' style={{ color: '#fff', fontWeight: 'bold' }}>
-        Search Unsplash Images
-      </h1>
-      {children}
+        <h1 className='text-white text-center text-2xl font-bold mb-5'>Find Images</h1>
+        {children}
+      </div>
     </div>
-    </div>
-  );
-};
+  )
+}
 
-export default bar;
+export default Bar
